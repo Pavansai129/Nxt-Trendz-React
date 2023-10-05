93ddf673176c68efe9d087d0a85c89da50e1525d
pavansai <pavansaivvit@gmail.com>
2023-10-05 12:10:16 +0000

    Nxt Trendz code reviewed

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,10 +2,7 @@ import CartContext from '../../context/CartContext'
 
 import Header from '../Header'
 import EmptyCartView from '../EmptyCartView'
-<<<<<<< HEAD
 import CartListView from '../CartListView'
-=======
->>>>>>> 261b097d39c63b16189b67f6a039a865eb85a7d8
 import CartSummary from '../CartSummary'
 
 import './index.css'
@@ -15,12 +12,7 @@ const Cart = () => (
     {value => {
       const {cartList, removeAllCartItems} = value
       const showEmptyView = cartList.length === 0
-<<<<<<< HEAD
       const onClickRemoveAllBtn = () => {
-=======
-      // TODO: Update the functionality to remove all the items in the cart
-      const removeAllItems = () => {
->>>>>>> 261b097d39c63b16189b67f6a039a865eb85a7d8
         removeAllCartItems()
       }
 
